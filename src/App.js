7eb948bb9c367f0c79489da7471447f7eea243e2
Rekha Shinde <rekha.shinde@angularminds.in>
2023-06-13 12:35:24 +0530

    push panorbit assignment

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Gallery from "./routes/Gallery";
+import LandingPage from "./routes/LandingPage";
+import Posts from "./routes/Posts";
+import Profile from "./routes/Profile";
+import ProfileHomePage from "./routes/ProfileHomePage";
+import ToDo from "./routes/ToDo";
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        {/* Route for the landing page */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Route for the user's profile homepage */}
+        <Route path="/profile/:userId" element={<ProfileHomePage />} />
+
+        {/* Route for the user's profile */}
+        <Route path="/profile/:userId" element={<Profile />} />
+
+        {/* Route for the user's posts */}
+        <Route path="/profile/posts" element={<Posts />} />
+
+        {/* Route for the user's gallery */}
+        <Route path="/profile/gallery" element={<Gallery />} />
+
+        {/* Route for the user's to-do list */}
+        <Route path="/profile/todo" element={<ToDo />} />
+
+        {/* Route for any other URL */}
+        <Route
+          path="*"
+          element={
+            <img
+              style={{ margin: "auto" }}
+              src="https://i.pinimg.com/originals/11/dc/96/11dc96d2e4daca3ea8ff6aa41299b5e1.gif"
+              alt=""
+            />
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
